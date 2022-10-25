e59dfc3ace1d33b13bcb1f2ce92698ebd183c170
Koo <31850425+gen-com@users.noreply.github.com>
2022-10-25 14:29:02 +0900

    Fix size variable to count variable

--- Queue/ArrayQueue/ArrayQueue/main.swift
+++ Queue/ArrayQueue/ArrayQueue/main.swift
@@ -11,15 +11,15 @@ struct Queue<T> {
     private var frontIndex: Int
     /// 가장 뒤의 원소를 가리키는 인덱스.
     private var backIndex: Int
-    /// 배열의 크기.
-    private let size: Int
+    /// 현재 큐에 있는 원소들의 수.
+    private(set) var count: Int
     /// 저장공간 배열.
     private var array: Array<T?>
     
     init(size: Int) {
         frontIndex = 0
         backIndex = 0
-        self.size = size
+        count = 0
         array = Array(repeating: nil, count: size)
     }
     
@@ -30,7 +30,7 @@ struct Queue<T> {
     
     /// 큐가 가득차 있는지 여부.
     var isFull: Bool {
-        (backIndex + 1) % size == frontIndex
+        (backIndex + 1) % array.count == frontIndex
     }
     
     /// 가장 앞의 원소.
@@ -48,9 +48,10 @@ struct Queue<T> {
         if isFull { return }
         if isEmpty == false {
             backIndex += 1
-            backIndex %= size
+            backIndex %= array.count
         }
         array[backIndex] = data
+        count += 1
     }
     
     /// 큐의 제일 앞의 원소를 반환합니다.
@@ -60,8 +61,9 @@ struct Queue<T> {
         array[frontIndex] = nil
         if isEmpty == false {
             frontIndex += 1
-            frontIndex %= size
+            frontIndex %= array.count
         }
+        count -= 1
         return front
     }
 }
